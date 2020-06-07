a5200c5130e9a9af62547ce632455567eaa3cdeb
Ryo Tanaka <neko.jekyll@gmail.com>
2020-06-07 16:29:03 +0900

    paletteはimportを利用して実装、ディレクトリ構成を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-import '../css/_palette.scss'
-import '../css/_layout.scss'
 import '../css/application.scss'
 
 require("@rails/ujs").start()
